b8cb63aeb036a306a3e2123f8c737382d58da96d
s-kan <gkswltjr00@gmail.com>
2022-12-24 23:56:23 +0900

    2022-12-24 solved:1

---
+++ 1026_보물.py
@@ -0,0 +1,8 @@
+n = int(input())
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+result = 0
+for i in range(n):
+    result += a[i]*b[i]
+print(result)
