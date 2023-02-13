9be85e0122d19edb1223eae1eaeb2e5fbdef9362
s-kan <gkswltjr00@gmail.com>
2023-02-13 22:38:49 +0900

    2023-02-13 solved:1

--- 1912_연속합.py
+++ 1912_연속합.py
@@ -1,7 +1,13 @@
 n = int(input())
 arr = list(map(int, input().split()))
-arr1 = []
-tmp = 0
-for i in range(n):
-    arr1.append(sum(arr[:i]))
-print(arr1)
+dp = []
+dp.append(arr[0])
+tmp = arr[0]
+for i in range(1, n):
+    tmp += arr[i]
+    if arr[i] < tmp:
+        dp.append(tmp)
+    else:
+        dp.append(arr[i])
+        tmp = arr[i]
+print(max(dp))
